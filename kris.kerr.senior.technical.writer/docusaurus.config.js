4fcc8e92c7e353df0fab5892ae5008c9e66ad37b
Kris Kerr <kkerr99@gmail.com>
2026-02-16 17:49:37 -0800

    Updated GitHub link on main page horizontal navigation bar to point to my GitHub repo where site concent is stored.

--- kris.kerr.senior.technical.writer/docusaurus.config.js
+++ kris.kerr.senior.technical.writer/docusaurus.config.js
@@ -100,7 +100,8 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            // href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/kkerr99/kris.kerr.professional.website',
             label: 'GitHub',
             position: 'right',
           },
